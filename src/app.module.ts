cf46e5f8136e0e319b2377c54cc2bc1ce261f516
davi-pc <davimatosc@hotmail.com>
2026-03-16 16:13:09 -0300

    Sqlite configure, transaction service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionsService } from './modules/trasactions/transactions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [Transaction],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TransactionsService],
 })
 export class AppModule {}
