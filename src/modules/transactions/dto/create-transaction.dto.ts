ae95935df56941662dc191a9e7e4d42693c3e58d
davi-pc <davimatosc@hotmail.com>
2026-03-25 12:03:50 -0300

    Handle validation exceptions

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,14 +1,23 @@
-import { IsEnum, IsNumber, IsString, IsUUID } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+} from 'class-validator';
 import { TransactionType } from '../enums/transaction-type';
 
 export class CreateTransactionDto {
   @IsUUID()
+  @IsNotEmpty()
   invoiceId: string;
 
   @IsEnum(TransactionType)
   type: TransactionType;
 
   @IsNumber()
+  @IsPositive()
   amount: number;
 
   @IsString()
